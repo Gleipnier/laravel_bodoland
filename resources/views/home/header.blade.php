58a59a8f589710fef6c87d10256c90e89869581a
Gleipnier <112313481+Gleipnier@users.noreply.github.com>
2024-09-06 19:01:59 +0530

    mobile navbar highlight

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -1,7 +1,7 @@
 <div class="header_main">
             <div class="mobile_menu">
                
-               <div style="border: 3px solid #454545; background-color:#454545; padding: 10px; position: fixed; z-index: 1000;">
+               <div style="border: 3px solid #faf0e6; background-color:#faf0e6; padding: 10px; position: fixed; z-index: 1000;">
                <nav class="navbar navbar-expand-lg navbar-light bg-light">
                   <div class="logo_mobile"><a href=""><img src="images/logo.png"></a></div>
                   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
